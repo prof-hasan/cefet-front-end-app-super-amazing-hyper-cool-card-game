08382b38674932fefd4243e96ef6fc2a0e3feaa1
ArthurH54 <arthurhenriquesky22@gmail.com>
2025-12-02 14:32:26 -0300

    Update jogo.js

--- public/jogo/jogo.js
+++ public/jogo/jogo.js
@@ -1,8 +1,32 @@
-const socket = io('ws://localhost:8080');
+window.addEventListener("load", () => {
+    const canvas = document.getElementById("arena");
 
-socket.on('turno', () => {
-    
+    if(canvas.getContext) {
+        /* Canvas */
+        // Setup
+        const ctx = canvas.getContext("2d");
+        
+        // Fundo
+        const img = new Image();
+        img.addEventListener("load", () => { ctx.drawImage(img, 0, 0); });
+        img.src = "arena.jpeg";
+
+
+
+
+        //////
+        /* Servidor */ 
+        const socket = io('ws://localhost:8080');
+
+        socket.on('turno', () => {
+            
+        });
+
+        let deck1 = [];
+        let deck2 = [];
+
+    } 
+
+    else console.log("Desculpe, seu navegador não é compatível com <canvas> :(");
+
 });
-
-let deck1 = [];
-let deck2 = [];
